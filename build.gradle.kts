c8b8297d1f12de2192e773a39c277a3b091dd818
Ilan Goldenstein <angeli106@gmail.com>
2019-02-28 16:04:47 +0200

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,39 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+version = "0.1-SNAPSHOT"
+
 plugins {
-    `java-library`
     kotlin("jvm") version "1.3.21"
+    `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "0.10.1"
 }
 
 repositories {
-    mavenCentral()
+    jcenter()
 }
-group = "org.sspiderd"
-version = "1.0-SNAPSHOT"
 
 dependencies {
     implementation(gradleApi())
     implementation(kotlin("stdlib-jdk8"))
 }
+
+gradlePlugin {
+    plugins {
+        create("testSplitterPlugin") {
+            id = "org.sspiderd.testsplitter"
+            displayName = "Test Splitter Plugin"
+            description = "This plugin splits tests for gradle into individual file to allow concurrent testing"
+            implementationClass = "org.sspiderd.testsplitter.TestSplitterPlugin"
+        }
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/sspiderd/test-splitter-gradle-plugin.git"
+    vcsUrl = "https://github.com/sspiderd/test-splitter-gradle-plugin.git"
+    tags = listOf("test", "tests", "split")
+}
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
